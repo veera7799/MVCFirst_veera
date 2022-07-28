2cac597716f0a9ce9f28a597e0a32c5bdf356d3e
veera <naga.devops99@gmail.com>
2022-07-28 19:23:43 +0530

    first
    my first commit

---
+++ packages/MVCFirstProject/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCFirstProject.Models;
+
+namespace MVCFirstProject.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET:Employee
+        public ActionResult EmployeeInfo()
+            {
+            Employee employee = new Employee()
+            {
+                EmployeeId = 1001,
+                EmployeeName = "Veera Kasani",
+                EmployeeLocation = "Banglore"
+            };
+
+
+            return View(employee);
+
+            }
+        }
+    }
---
+++ packages/MVCFirstProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCFirstProject.MvcApplication" Language="C#" %>
